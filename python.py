db3d94fc857cb7cbb12b0bf0bfe7c0593ec1293c
Gvn <122465852+usarnameMe@users.noreply.github.com>
2024-05-15 14:10:12 +0400

    Done

---
+++ python.py
@@ -0,0 +1,21 @@
+def convert_fun(input_value):
+    if isinstance(input_value, str):
+        try:
+            return int(input_value)
+        except ValueError:
+            return "Error: Cannot convert string to integer."
+    elif isinstance(input_value, int):
+        return str(input_value)
+    else:
+        return "Error: This type is not supported! Try another one!"
+
+
+user_input = input("Enter a value: ")
+
+try:
+    user_input = int(user_input)
+except ValueError:
+    pass
+
+converted_value = convert_fun(user_input)
+print(f"Converted value: {converted_value}")
